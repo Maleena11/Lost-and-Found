4c6f5200b480e959bc193a7f0b3b238a1e7f404f
Yasassri <ekanayakayasassri@gmail.com>
2026-03-26 20:46:07 +0530

    feat(hero): connect homepage search to notice board with auto-triggered query

--- src/shared/components/Hero.jsx
+++ src/shared/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import BackgroundCarousel from "./BackgroundCarousel";
 import "../../Hero.css";
 
 export default function Hero() {

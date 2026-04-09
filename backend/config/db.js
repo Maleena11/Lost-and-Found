fdf729a4c7627ed7e1be06a44963b6b8f4e7d25d
YasassriThamaliEkanayaka <ekanayakayasassri@gmail.com>
2026-04-09 09:18:40 +0530

    feat(notice): Updated db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,6 @@
 // config/db.js
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 // Load environment variables
 dotenv.config();
@@ -16,7 +14,7 @@ const connection = async () => {
       throw new Error("MONGO_URI is not defined in .env");
     }
 
-    await mongoose.connect(dburl, { family: 4 });
+    await mongoose.connect(dburl);
 
     console.log("✅ MongoDB Connected successfully");
   } catch (e) {
@@ -26,3 +24,4 @@ const connection = async () => {
 };
 
 module.exports = connection;
+
